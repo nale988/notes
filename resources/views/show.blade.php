ea9158141c5ce52e771acb7568dff8e501e81c29
nale988 <65071690+nale988@users.noreply.github.com>
2020-09-07 07:20:21 +0200

    Update show.blade.php

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -13,8 +13,7 @@
             </a>
         </span>
     </div>
-    {{-- <div class="card-body"><span class="flex-nowrap"{!! $note -> note !!}</span></div> --}}
-    <div class="card-body"><textarea rows="8">{!! $note -> note !!}</textarea>
+    <div class="card-body"><span class="flex-nowrap" style="overflow: auto;">{!! $note -> note !!}</span></div>
     <div class="card-footer bg-primary text-white">
         <div class="row">
             <div class="col">
