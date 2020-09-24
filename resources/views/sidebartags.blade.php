e85e92c903a0e60d552e271c40e69fccc4d62ec6
nale988 <65071690+nale988@users.noreply.github.com>
2020-09-24 15:23:26 +0200

    Update sidebartags.blade.php

--- resources/views/sidebartags.blade.php
+++ resources/views/sidebartags.blade.php
@@ -2,7 +2,7 @@
     @foreach($tags as $tag)
     @if(count($tag -> notes) > 0)
     <li class="list-group-item list-group-item-action">
-        <div class=" d-flex justify-content-between align-items-center">
+        <div class="d-flex justify-content-between align-items-left">
             <a class="dropdown-toggle" data-toggle="collapse" style="text-decoration: none; color: #000;" href="#c{{ preg_replace('/[^a-z0-9.]+/i', '-', $tag -> tag) }}" role="button" aria-expanded="false" aria-controls="c{{ preg_replace('/[^a-z0-9.]+/i', '-', $tag -> tag) }}">
                 {{ $tag -> tag }}
             </a>
@@ -10,9 +10,9 @@
         </div>
 
         <div class="collapse" id="c{{ preg_replace('/[^a-z0-9.]+/i', '-', $tag -> tag) }}">
-            <ul class="list-group">
+            <ul class="list-unstyled">
             @foreach($tag -> notes as $note)
-                <li class="list-group-item list-group-item-action">
+                <li>
                     <a class="btn btn-link text-left" href="{{ route('show', [$note -> id, 1]) }}" title="{{ $note -> title }}">
                         @if(strlen($note -> title) > 20)
                             {{ substr($note -> title, 0, 20) }}...
